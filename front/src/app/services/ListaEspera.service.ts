e63835c2496895b08c05a0dc6cb28ae0462a42f6
Bill Saravia <bill.saravia@tecsup.edu.pe>
2023-06-22 12:11:53 -0500

    first commit

--- front/src/app/services/ListaEspera.service.ts
+++ front/src/app/services/ListaEspera.service.ts
@@ -1,36 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Producto } from '../models/producto';
+import { ListaEspera } from '../models/ListaEspera';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductoService {
-  url = 'http://localhost:4000/api/productos/';
+export class ListaEsperaService {
+  url = 'http://localhost:4000/api/ListaEspera/';
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient) { }
 
-  }
-
-  getProductos(): Observable<any> {
+  getListaEspera(): Observable<any> {
     return this.http.get(this.url);
   }
 
-  deleteProducto(id: string): Observable<any> {
+  deleteListaEspera(id: string): Observable<any> {
     return this.http.delete(this.url + id);
   }
 
-  guardarProducto(producto: Producto): Observable<any> {
-    return this.http.post(this.url, producto);
+  guardarListaEspera(listaEspera: ListaEspera): Observable<any> {
+    return this.http.post(this.url, listaEspera);
   }
 
-  viewProducto(id?: string): Observable<any> {
-    return this.http.get(this.url + id)
+  getListaEsperaPelicula(idPelicula: string): Observable<any> {
+    return this.http.get(this.url + 'pelicula/' + idPelicula);
   }
 
-  actualizarProducto(id: string, producto: Producto): Observable<any> {
-    return this.http.put(this.url + id, producto);
+  agregarSocioALista(idLista: string, idSocio: string): Observable<any> {
+    return this.http.put(this.url + idLista + '/agregar-socio/' + idSocio, {});
   }
 
+  eliminarSocioDeLista(idLista: string, idSocio: string): Observable<any> {
+    return this.http.put(this.url + idLista + '/eliminar-socio/' + idSocio, {});
+  }
 }
