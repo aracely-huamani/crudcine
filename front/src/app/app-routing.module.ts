ab8c6d35dafabc93c8527a85dc14c31081393a62
Bill Saravia <bill.saravia@tecsup.edu.pe>
2023-06-22 12:22:09 -0500

    first commit

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CrearProductosComponent } from './pages/productos/crear-productos/crear-productos.component';
+import { CrearPeliculaComponent } from './pages/pelicula/crear-peliculas/crear-peliculas.component';
 import { EditarProductosComponent } from './pages/productos/editar-productos/editar-productos.component';
 import { ListarProductosComponent } from './pages/productos/listar-productos/listar-productos.component';
 import { CreateUserComponent } from './pages/users/create-user/create-user.component';
@@ -12,15 +12,17 @@ import { EditarTiendasComponent } from './pages/tiendas/editar-tiendas/editar-ti
 const routesInicio: Routes = [
   { path: '', component: LoginComponent },
   { path: 'crear-usuario', component: CreateUserComponent },
-  { path: 'listar-productos', component: ListarProductosComponent },
-  { path: 'crear-productos', component: CrearProductosComponent },
-  { path: 'editar-producto/:id', component: EditarProductosComponent },
-  { path: 'tiendas', component: TiendasComponent },
-  { path: 'crear-tiendas', component: CrearTiendasComponent},
-  { path: 'editar-tienda/:id', component: EditarTiendasComponent},
-  { path: '**', redirectTo: '', pathMatch: 'full'}
+  { path: 'listar-peliculas', component: ListarPeliculasComponent },
+  { path: 'crear-pelicula', component: CrearPeliculaComponent },
+  { path: 'editar-pelicula/:id', component: EditarPeliculaComponent },
+  { path: 'listar-prestamos', component: ListarPrestamosComponent },
+  { path: 'crear-prestamo', component: CrearPrestamoComponent },
+  { path: 'editar-prestamo/:id', component: EditarPrestamoComponent },
+  { path: 'listar-lista-espera', component: ListarListaEsperaComponent },
+  { path: 'crear-lista-espera', component: CrearListaEsperaComponent },
+  { path: 'editar-lista-espera/:id', component: EditarListaEsperaComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routesInicio)],
