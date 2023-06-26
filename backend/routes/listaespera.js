f50a0998350835769e0632032997dadddb1130b5
aracely-huamani <102335298+aracely-huamani@users.noreply.github.com>
2023-06-26 15:35:05 -0500

    change

--- backend/routes/listaespera.js
+++ backend/routes/listaespera.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const productoController = require('../controllers/listaesperaController');
 
+
+router.post('/', listaesperaController.crearListaespera);
+router.get('/', listaesperaController.obtenerListaespera);
+router.put('/:id', listaesperaController.actualizarListaespera);
+router.get('/:id', listaesperaController.verListaespera);
+router.delete('/:id', listaesperaController.eliminarListaespera);
+
+module.exports = router;
